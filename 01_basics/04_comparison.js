e3f95d7d7fdcfc62f17405dcb409d2c573e7afc1
vaibhav-sm22 <103096441+vaibhav-sm22@users.noreply.github.com>
2025-03-06 10:06:16 +0000

    Done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1)
+
+// console.log("2" > 1); // when you compare two values make sure it is of same datatype, typesript is strict language
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); comparison and equality check works different in JacaScript
+
+
+//=== is strict check 
+
+console.log("2" === 2)
+
+// summary is during comparison when datatype is same then there is no issue, when datatype is changed then JS behaves differently and if datatype is different then
+// use strict check
