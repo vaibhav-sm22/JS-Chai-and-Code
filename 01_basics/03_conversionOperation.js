f29bd9f09be50c22fc77e282075015ddb2bda5e0
vaibhav-sm22 <103096441+vaibhav-sm22@users.noreply.github.com>
2025-03-06 11:52:16 +0000

    Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,5 +11,37 @@ let valueNumber = Number(score)
 let isLoggenIn = 1
 let booleanIsLoggedIn = Boolean(isLoggenIn)
 
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// ******* Operations *******
+
+let value = 3
+let negValue = -value
+// console.log(value)
+
+// console.log(2**3)//raise to the power
+// console.log(2+2)
+
+let str1 = "hello"
+let str2 = "vaibhav"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(2+"2")  22
+// console.log(1+2+"2")  32
+// console.log(1+"2"+1)  121
+
+//console.log(+true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.table([num1,num2,num3])
+
+let gameCounter = 100
+++gameCounter;//prefix operator postfix operator learn from mdn 
+console.log(gameCounter);
+
