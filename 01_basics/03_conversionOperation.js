b1ef15f641f20c99bf9aea2deaf9fd36735ae4f6
vaibhav-sm22 <103096441+vaibhav-sm22@users.noreply.github.com>
2025-03-06 07:02:24 +0000

    Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,15 @@
-let score = 33
+let score = true
 
 console.log(typeof score)
+console.log(typeof(score))
+
+let valueNumber = Number(score)
+
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+let isLoggenIn = 1
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
