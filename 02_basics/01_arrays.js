cac2ff0fae9b9f8cbbeea0ceba76874490600108
vaibhav-sm22 <103096441+vaibhav-sm22@users.noreply.github.com>
2025-03-18 16:29:27 +0000

    doen

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+let myHeroes = ["Avengers","Hulk","IronMan"]
+
+const myArr2 = new Array()
+console.log(myArr[1]);
+
+// Array Methods, using investigating study like using console.log
+
+// myArr.push(6)
+// myArr.push(6)
+// myArr.pop // will remove last value
+
+//myArr.unshift(9)// will insert element in first index, not a good practice only use when necessary 
+myArr.shift()// will remove first element from the array    
+
+console.log(myArr.includes(9));// to check if element present or not just a simple question
+
+
+console.log(myArr)
+log
+
+
